53dcdd63e763140ee48491c49e5eaffe7e89c566
Mahdi <mata1381@gmail.com>
2025-03-09 02:06:36 +0330

    start the project

---
+++ srunner/srunner.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+const defaultPort = 9999
+
+func main() {
+	// Initialize the key-value store.
+	store, _ := kvstore.CreateWithBackdoor()
+	// Initialize the server.
+	server := p0partA.New(store)
+	if server == nil {
+		fmt.Println("New() returned a nil server. Exiting...")
+		return
+	}
+
+	// Start the server and continue listening for client connections in the background.
+	if err := server.Start(defaultPort); err != nil {
+		fmt.Printf("KeyValueServer could not be started: %s\n", err)
+		return
+	}
+
+	fmt.Printf("Started KeyValueServer on port %d...\n", defaultPort)
+
+	// Block forever.
+	select {}
+}
